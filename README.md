ab790b66455336cc9a2cb2243029327b868ee419
jstzwj <1103870790@qq.com>
2023-06-07 22:09:15 +0800

    init repo

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 ---
 license: apache-2.0
 ---
+
+Shami Tokenizer
+===
+
+This is the tokenizer of Shami Model.
---
+++ train_tokenizer.py
@@ -0,0 +1,28 @@
+import json
+
+from transformers import AutoTokenizer
+old_tokenizer = AutoTokenizer.from_pretrained("gpt2")
+
+import os
+from datasets import load_dataset
+
+langs = ["en", "ja", "ko", "zh-cn", "zh-tw"]
+raw_datasets = [
+    load_dataset("wiki40b", lang, beam_runner='DirectRunner')
+    for lang in langs
+]
+
+total_line = 0
+for training_dataset in raw_datasets:
+    for line in training_dataset["train"]:
+        total_line += 1
+
+def training_dataset_iterator():
+    for training_dataset in raw_datasets:
+        for line in training_dataset["train"]:
+            yield line['text']
+
+# tokenizer.train(training_files, trainer)
+tokenizer = old_tokenizer.train_new_from_iterator(training_dataset_iterator(), 102000, total_line)
+
+tokenizer.save_pretrained("tokenizer-shami")
